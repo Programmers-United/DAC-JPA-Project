bd67836606353639b3643de044e0515bd1954c20
João Darwin <joaodarwin.ip22@gmail.com>
2024-09-03 22:27:42 -0300

    feat: Fazer persistência para teste

--- src/main/java/br/edu/ifpb/exemplosjpa/loader/Loader.java
+++ src/main/java/br/edu/ifpb/exemplosjpa/loader/Loader.java
@@ -1,5 +1,6 @@
 package br.edu.ifpb.exemplosjpa.loader;
 
+import br.edu.ifpb.exemplosjpa.models.Test;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.slf4j.Logger;
@@ -7,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Component
 public class Loader implements CommandLineRunner {
@@ -24,6 +27,12 @@ public class Loader implements CommandLineRunner {
     @Transactional
     public void run(String... args) throws Exception {
         // Coloque o seu código aqui
+        Test test = new Test();
+
+        test.setName("Foo bar");
+        test.setValue(BigDecimal.valueOf(15.134));
+
+        entityManager.persist(test);
     }
 
 }
