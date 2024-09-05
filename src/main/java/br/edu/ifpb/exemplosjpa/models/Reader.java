e45f527e1ef5a64d0d0e958963f2062b66dcff37
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-09-05 10:05:10 -0300

    feat: Creating entity Raeder that extends Person

--- src/main/java/br/edu/ifpb/exemplosjpa/models/Reader.java
+++ src/main/java/br/edu/ifpb/exemplosjpa/models/Reader.java
@@ -1,4 +1,21 @@
 package br.edu.ifpb.exemplosjpa.models;
 
-public class Reader {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Reader extends Person {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
